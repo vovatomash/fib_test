a9dfcd45f2f3504bec430512addf473740d295d0
Volodymyr Tomash <vtomash@odl1701003.lohika.com>
2018-05-17 15:19:53 +0300

    #init commit

---
+++ fibo.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import sys
+import os
+import time
+import argparse
+
+
+def main_recursive(max_iter_number):
+    for x in range(max_iter_number):
+        print "fibonacci(%d) = %d" % (x, fibonacci(x))
+
+
+# RECURSIVE
+def fibonacci(n):
+    if n < 2:
+        return n
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
+
+def main_generator():
+    f = fiboyield()
+    for x in range(60):
+        print "fibonacci: %d" % f.next()
+
+
+# GENERATOR
+def fiboyield():
+    x, y = 0, 1
+    while True:
+        yield x
+        x, y = y, x + y
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--count',
+        dest='fib_count',
+        action='store',
+        type=int,
+        default=0,
+        help='30 + count will get fibonacci last iteration number')
+    args = parser.parse_args()
+    start_time = time.time()
+    main_recursive(args.fib_count + 31)
+    print time.time() - start_time, "seconds"
