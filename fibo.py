3567629160a817539f2005331653656f55e0c94c
vovatomash <39334889+vovatomash@users.noreply.github.com>
2019-10-29 18:38:12 +0200

    Update fibo.py
    as

--- fibo.py
+++ fibo.py
@@ -9,7 +9,7 @@ import argparse
 
 def main_recursive(max_iter_number):
     for x in range(max_iter_number):
-        print("fibonacci({}) = {}".format(x, fibonacci(x))
+        print("fibonacci({} -- 22) = {}".format(x, fibonacci(x))
 
 
 # RECURSIVE
